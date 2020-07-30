af3cecc2f10e0bcb3799af447e66029316f1b5c3
Shivam <shivambehal@yahoo.com>
2020-07-30 21:52:57 +0530

    react test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye
+          CI CD Test Sucess
         </p>
         <a
           className="App-link"
